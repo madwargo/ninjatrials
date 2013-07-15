4a682d12edab2cfb6fd528e5fd54916e4cf11fad
madwargo <lordwargo@gmail.com>
2013-07-15 10:21:16 +0200

    Setup inicial.
    Se clona el proyecto base de andengine, añadido gitignore, añadidos iconos de lanzamiento de la app.
     Configuradas las bibliotecas.
    
    Copiadas las clases manager (sin adaptar todavía a la ap) y userdata.

---
+++ src/com/madgear/ninjatrials/MainActivity.java
@@ -0,0 +1,59 @@
+package com.madgear.ninjatrials;
+
+import java.io.IOException;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
+import com.madgear.pruebasandengine.R;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends BaseGameActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreateResources(
+			OnCreateResourcesCallback pOnCreateResourcesCallback)
+			throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
+			throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPopulateScene(Scene pScene,
+			OnPopulateSceneCallback pOnPopulateSceneCallback)
+			throws IOException {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
